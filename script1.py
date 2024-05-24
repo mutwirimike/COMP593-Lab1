984396f9c5d7c2d8f367a637633a26550fb32fdd
mutwirimike <mikemutwiri@icloud.com>
2024-05-24 17:10:26 -0400

    Modified both files in the repo clone
    -Deleted a print statement to script1.py
    -Added a new print statement to script2.py

--- script1.py
+++ script1.py
@@ -1,3 +1,2 @@
 # print line from zen of python
-print("Unless explicitly silenced")
 print("In the face of ambiguity, refuse the temptation to guess.")
--- script2.py
+++ script2.py
@@ -1,3 +1,4 @@
 # This is a good advice
 print("beautiful is better than ugly")
 print("Although never is often better than *right* now")
+print("python is fun")
